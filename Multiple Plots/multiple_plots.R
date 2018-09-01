bb0433bc987fa7cdff82c709f8893b0e9a8ba43d
Sushmita Kaneri <sushmita.kaneri17@vit.edu>
2018-09-01 19:39:29 +0530

    Create multiple_plots.R

---
+++ Multiple Plots/multiple_plots.R
@@ -0,0 +1,21 @@
+# List all the graphical parameters that are currently active in your session, by running par().
+# Next, use par() to set the mfrow parameter: R should plot figures on a 2-by-1 grid (2 rows, 1 column).
+# Build two plots:
+# A scatterplot that plots the votes (x-axis) against the rating (y-axis) variable of movies.
+# A histogram of the votes variable
+
+
+
+
+# movies is pre-loaded in your workspace
+
+# List all the graphical parameters
+par()
+
+# Specify the mfrow parameter
+par(mfrow=c(2,1))
+
+# Build two plots
+plot(movies$votes,movies$rating)
+hist(movies$votes)
+
