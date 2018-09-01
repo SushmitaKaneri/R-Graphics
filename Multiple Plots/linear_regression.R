1b04706d17e1b908a7cc19dbb0113f9ea7c33692
Sushmita Kaneri <sushmita.kaneri17@vit.edu>
2018-09-01 20:06:39 +0530

    Create linear_regression.R

---
+++ Multiple Plots/linear_regression.R
@@ -0,0 +1,18 @@
+# Fit a linear regression that models rating based on votes. Use the function lm() with movies$rating ~ movies$votes as the only argument. Assign the result to movies_lm.
+# Build a scatterplot with votes on the x-axis and rating on th y-axis.
+# Add a straight line to this plot with abline(). You have to pass the coefficients of movies_lm to it. You can use coef() to extract these coefficients.
+
+
+
+
+# movies is pre-loaded in your workspace
+
+# Fit a linear regression: movies_lm
+movies_lm <- lm(movies$rating ~ movies$votes)
+
+# Build a scatterplot: rating versus votes
+plot(movies$votes, movies$rating)
+
+
+# Add straight line to scatterplot
+abline(coef(movies_lm))
